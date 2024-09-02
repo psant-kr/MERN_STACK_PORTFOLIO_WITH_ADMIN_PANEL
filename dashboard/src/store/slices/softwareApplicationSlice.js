1345f334909b83b50f9089632fde5c841296d533
prashant kumar <prashantdoa1996@gmail.com>
2024-09-03 04:51:38 +0530

    only render backend deployed file is working

--- dashboard/src/store/slices/softwareApplicationSlice.js
+++ dashboard/src/store/slices/softwareApplicationSlice.js
@@ -74,7 +74,7 @@ export const getAllSoftwareApplications = () => async (dispatch) => {
   );
   try {
     const response = await axios.get(
-      "https://mern-stack-portfolio-with-admin-panel-backend.vercel.app/api/v1/softwareapplication/getall",
+      "https://mern-stack-portfolio-with-admin-panel.onrender.com/api/v1/softwareapplication/getall",
       { withCredentials: true }
     );
     dispatch(
@@ -98,7 +98,7 @@ export const addNewSoftwareApplication = (data) => async (dispatch) => {
   );
   try {
     const response = await axios.post(
-      "https://mern-stack-portfolio-with-admin-panel-backend.vercel.app/api/v1/softwareapplication/add",
+      "https://mern-stack-portfolio-with-admin-panel.onrender.com/api/v1/softwareapplication/add",
       data,
       {
         withCredentials: true,
@@ -126,7 +126,7 @@ export const deleteSoftwareApplication = (id) => async (dispatch) => {
   );
   try {
     const response = await axios.delete(
-      `https://mern-stack-portfolio-with-admin-panel-backend.vercel.app/api/v1/softwareapplication/delete/${id}`,
+      `https://mern-stack-portfolio-with-admin-panel.onrender.com/api/v1/softwareapplication/delete/${id}`,
       {
         withCredentials: true,
       }
